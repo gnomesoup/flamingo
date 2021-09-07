b6d2a2da15aaec9f73d631e4494acced8d738517
Michael Pfammatter <pfammatter@gmail.com>
2021-09-07 15:24:07 -0500

    Prefix Family Name working version

--- Flamingo.extension/Flamingo.tab/Manage.panel/Prefix Family Names.pushbutton/script.py
+++ Flamingo.extension/Flamingo.tab/Manage.panel/Prefix Family Names.pushbutton/script.py
@@ -1,33 +1,51 @@
-from pyrevit import DB, forms, HOST_APP, clr
+from pyrevit import DB, forms, HOST_APP, revit
 
-clr.AddReference("RevitAPI")
-import Autodesk.Revit.DB as DBX
 doc = HOST_APP.doc
 
-categories = [
-    DB.BuiltInCategory.OST_Casework,
-    DB.BuiltInCategory.OST_ElectricalEquipment,
-    DB.BuiltInCategory.OST_ElectricalFixtures,
-    DB.BuiltInCategory.OST_Entourage,
-    DB.BuiltInCategory.OST_Furniture,
-    DB.BuiltInCategory.OST_FurnitureSystems,
-    DB.BuiltInCategory.OST_GenericModel,
-    # DB.BuiltInCategory.OST_LightingDevices,
-    DB.BuiltInCategory.OST_LightingFixtures,
-    DB.BuiltInCategory.OST_MechanicalEquipment,
-    DB.BuiltInCategory.OST_Parking,
-    DB.BuiltInCategory.OST_Planting,
-    DB.BuiltInCategory.OST_PlumbingFixtures,
-    DB.BuiltInCategory.OST_Site,
-    DB.BuiltInCategory.OST_SpecialityEquipment,
-
-]
+categoryBuiltIns = {
+    DB.BuiltInCategory.OST_Casework: "CW_",
+    DB.BuiltInCategory.OST_ElectricalEquipment: "EL_",
+    DB.BuiltInCategory.OST_ElectricalFixtures: "EF_",
+    DB.BuiltInCategory.OST_Entourage: "EL_",
+    DB.BuiltInCategory.OST_Furniture: "FN_",
+    DB.BuiltInCategory.OST_FurnitureSystems: "FS_",
+    DB.BuiltInCategory.OST_GenericModel: "GM_",
+    DB.BuiltInCategory.OST_LightingFixtures: "LF_",
+    DB.BuiltInCategory.OST_MechanicalEquipment: "ME_",
+    DB.BuiltInCategory.OST_Parking: "PK_",
+    DB.BuiltInCategory.OST_Planting: "PL_",
+    DB.BuiltInCategory.OST_PlumbingFixtures: "PL_",
+    DB.BuiltInCategory.OST_RailingSupport: "RS_",
+    DB.BuiltInCategory.OST_RailingTermination: "RT_",
+    DB.BuiltInCategory.OST_Site: "SI_",
+    DB.BuiltInCategory.OST_SpecialityEquipment: "SE_",
+}
 
 families = DB.FilteredElementCollector(doc) \
-    .OfClass(DB.FamilySymbol) \
-    .WhereElementIsElementType()
+    .OfClass(DB.Family) \
+    # .WhereElementIsElementType()
 
-print("Count: {}".format(families.GetElementCount()))
+print("Unfiltered Count: {}\n".format(families.GetElementCount()))
 
-for family in families:
-    print(family.)
+with revit.Transaction("Rename Families By Category"):
+    for family in families:
+        for category in categoryBuiltIns.keys():
+            familyCategory = family.FamilyCategory
+            if familyCategory.Id.IntegerValue == category.value__:
+                prefix = categoryBuiltIns[category]
+                if (family.Name).startswith(prefix):
+                    print(
+                        "Family: {} :)".format(
+                            family.Name,
+                        )
+                    )
+                    continue
+                newName =  prefix + family.Name
+                print(
+                    "Family: {} -> {}".format(
+                        family.Name,
+                        newName
+                    )
+                )
+                family.Name = newName
+                break
