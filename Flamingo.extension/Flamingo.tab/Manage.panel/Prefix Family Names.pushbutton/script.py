662035dcb8728fd5efc957f62fddab62eb0def2f
Michael Pfammatter <pfammatter@gmail.com>
2021-09-07 18:31:27 -0500

    Prefix Family Name better messaging

--- Flamingo.extension/Flamingo.tab/Manage.panel/Prefix Family Names.pushbutton/script.py
+++ Flamingo.extension/Flamingo.tab/Manage.panel/Prefix Family Names.pushbutton/script.py
@@ -1,51 +1,67 @@
 from pyrevit import DB, forms, HOST_APP, revit
 
-doc = HOST_APP.doc
 
-categoryBuiltIns = {
-    DB.BuiltInCategory.OST_Casework: "CW_",
-    DB.BuiltInCategory.OST_ElectricalEquipment: "EL_",
-    DB.BuiltInCategory.OST_ElectricalFixtures: "EF_",
-    DB.BuiltInCategory.OST_Entourage: "EL_",
-    DB.BuiltInCategory.OST_Furniture: "FN_",
-    DB.BuiltInCategory.OST_FurnitureSystems: "FS_",
-    DB.BuiltInCategory.OST_GenericModel: "GM_",
-    DB.BuiltInCategory.OST_LightingFixtures: "LF_",
-    DB.BuiltInCategory.OST_MechanicalEquipment: "ME_",
-    DB.BuiltInCategory.OST_Parking: "PK_",
-    DB.BuiltInCategory.OST_Planting: "PL_",
-    DB.BuiltInCategory.OST_PlumbingFixtures: "PL_",
-    DB.BuiltInCategory.OST_RailingSupport: "RS_",
-    DB.BuiltInCategory.OST_RailingTermination: "RT_",
-    DB.BuiltInCategory.OST_Site: "SI_",
-    DB.BuiltInCategory.OST_SpecialityEquipment: "SE_",
-}
+if __name__ == "__main__":
+    doc = HOST_APP.doc
 
-families = DB.FilteredElementCollector(doc) \
-    .OfClass(DB.Family) \
-    # .WhereElementIsElementType()
+    categoryBuiltIns = {
+        DB.BuiltInCategory.OST_Casework: "CW_",
+        DB.BuiltInCategory.OST_DuctTerminal: "DT_",
+        DB.BuiltInCategory.OST_ElectricalEquipment: "EL_",
+        DB.BuiltInCategory.OST_ElectricalFixtures: "EF_",
+        DB.BuiltInCategory.OST_Entourage: "EN_",
+        DB.BuiltInCategory.OST_Furniture: "FN_",
+        DB.BuiltInCategory.OST_FurnitureSystems: "FS_",
+        DB.BuiltInCategory.OST_GenericModel: "GM_",
+        DB.BuiltInCategory.OST_LightingFixtures: "LF_",
+        DB.BuiltInCategory.OST_MechanicalEquipment: "ME_",
+        DB.BuiltInCategory.OST_Parking: "PK_",
+        DB.BuiltInCategory.OST_Planting: "PL_",
+        DB.BuiltInCategory.OST_PlumbingFixtures: "PL_",
+        DB.BuiltInCategory.OST_RailingSupport: "RS_",
+        DB.BuiltInCategory.OST_RailingTermination: "RT_",
+        DB.BuiltInCategory.OST_Site: "SI_",
+        DB.BuiltInCategory.OST_SpecialityEquipment: "SE_",
+        DB.BuiltInCategory.OST_Sprinklers: "SP_",
+        DB.BuiltInCategory.OST_StructConnections: "SC_",
+    }
 
-print("Unfiltered Count: {}\n".format(families.GetElementCount()))
+    families = DB.FilteredElementCollector(doc) \
+        .OfClass(DB.Family)
 
-with revit.Transaction("Rename Families By Category"):
+    familiesToRename = []
+    correctPrefixCount = 0
     for family in families:
         for category in categoryBuiltIns.keys():
             familyCategory = family.FamilyCategory
             if familyCategory.Id.IntegerValue == category.value__:
                 prefix = categoryBuiltIns[category]
                 if (family.Name).startswith(prefix):
-                    print(
-                        "Family: {} :)".format(
-                            family.Name,
-                        )
-                    )
-                    continue
-                newName =  prefix + family.Name
-                print(
-                    "Family: {} -> {}".format(
-                        family.Name,
-                        newName
-                    )
+                    correctPrefixCount += 1
+                    break
+                familiesToRename.append(
+                    {
+                        "family": family,
+                        "newName": prefix + family.Name,
+                    }
                 )
-                family.Name = newName
                 break
+
+    if len(familiesToRename) < 1:
+        familyString = "families" if correctPrefixCount > 1 else "family"
+        message = "{} {} had the correct prefix. No" \
+            " families required renaming.".format(
+                correctPrefixCount,
+                familyString
+            )
+        forms.alert(message)
+    else:
+        familyString = "families" if len(familiesToRename) > 1 else "family"
+        message = "Would you like to correct the prefix on {} {}?".format(
+            len(familiesToRename),
+            familyString
+        )
+        if forms.alert(message, yes=True, no=True):
+            with revit.Transaction("Rename families with category prefix"):
+                for item in familiesToRename:
+                    item['family'].Name = item['newName']
