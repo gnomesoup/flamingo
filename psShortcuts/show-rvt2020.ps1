ea7835a412a7be8431c533927b15804c265eff0b
Michael Pfammatter <pfammatter@gmail.com>
2021-09-09 22:08:17 -0500

    Add powershell script to check if revit is running

---
+++ psShortcuts/show-rvt2020.ps1
@@ -0,0 +1,25 @@
+function Show-Process($Process, [Switch]$Maximize)
+{
+  $sig = '
+    [DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+    [DllImport("user32.dll")] public static extern int SetForegroundWindow(IntPtr hwnd);
+  '
+  
+  if ($Maximize) { $Mode = 3 } else { $Mode = 4 }
+  $type = Add-Type -MemberDefinition $sig -Name WindowAPI -PassThru
+  $hwnd = $process.MainWindowHandle
+  $null = $type::ShowWindowAsync($hwnd, $Mode)
+  $null = $type::SetForegroundWindow($hwnd) 
+}
+
+$rvt = Get-Process -Name "revit" -ErrorAction Ignore | Where-Object {
+    $_.MainWindowTitle -like "Autodesk Revit 2020*"
+}
+
+if ($rvt) {
+    Show-Process($rvt)
+}
+else {
+    Start-Process -FilePath 'C:\Program Files\Autodesk\Revit 2020\Revit.exe'
+}
+
