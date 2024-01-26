dcb4a3d3116dde63cbf75b2018cb713a65cb6f63
Michael Pfammatter <pfammatter@gmail.com>
2024-01-26 10:22:50 -0600

    COBie CDx cleanup

--- Flamingo.extension/Flamingo.tab/COBie & CDX.panel/CDX.stack/COBie to CDX.pushbutton/script.py
+++ Flamingo.extension/Flamingo.tab/COBie & CDX.panel/CDX.stack/COBie to CDX.pushbutton/script.py
@@ -8,7 +8,6 @@ clr.ImportExtensions(System.Linq)
 
 if __name__ == "__main__":
     doc = HOST_APP.doc
-    # output = script.get_output()
 
     try:
         view = DB.FilteredElementCollector(doc)\
@@ -23,21 +22,4 @@ if __name__ == "__main__":
             exitscript=True
         )
     
-    # viewSchedules = DB.FilteredElementCollector(doc) \
-    #     .OfClass(DB.ViewSchedule) \
-    #     .ToElements()
-    # viewScheduleNames = [view.Name for view in viewSchedules]
-    # cdxScheduleViewData = {
-    #     "GSA.Floor": DB.ElementId(DB.BuiltInCategory.OST_Levels),
-    #     "GSA.Space (Rooms)": DB.ElementId(DB.BuiltInCategory.OST_Rooms),
-    #     "GSA.Asset (Type)": DB.ElementId.InvalidElementId,
-    #     "GSA.Asset (Instance)": DB.ElementId.InvalidElementId,
-    # }
-    # with revit.Transaction("Create CDX Schedules"):
-    #     for viewName, builtinCategory in cdxScheduleViewData.items():
-    #         if viewName not in viewScheduleNames:
-    #             CreateScheduleView(
-    #                 viewName,
-    #                 builtinCategory,
-    #             )
     COBieParametersToCDX(doc)
