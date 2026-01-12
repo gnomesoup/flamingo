e115f8fb735401e162cae963786b94bafdf4b1c7
Michael Pfammatter <pfammatter@gmail.com>
2026-01-12 09:42:28 -0600

    Schema settings tests update

--- Flamingo.extension/Flamingo.tab/Debug.panel/Tests.pulldown/Schema Tests.pushbutton/script.py
+++ Flamingo.extension/Flamingo.tab/Debug.panel/Tests.pulldown/Schema Tests.pushbutton/script.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     doc = HOST_APP.doc
     # LOGGER.set_debug_mode()
     try:
-        with revit.Transaction("Schema Test") as t:
+        with revit.Transaction("Schema Test Create"):
             try:
                 schema = extensible_storage.GetFlamingoSchema(doc)
                 projectInfo = doc.ProjectInformation
@@ -16,13 +16,14 @@ if __name__ == "__main__":
                 version = entity.Get[str](schema.GetField("Version"))
                 OUTPUT.print_md(
                     ":white_heavy_check_mark: GetFlamingoSchema succeeded. "
-                    "{} with version {}created/found.".format(
+                    "{} with version {} created or found.".format(
                         schema.SchemaName, version
                     )
                 )
             except Exception as e:
-                OUTPUT.print_md(":cross_mark: GetFlamingoSchema failed .\n{}".format(e))
+                OUTPUT.print_md(":cross_mark: GetFlamingoSchema failed. {}".format(e))
                 script.exit()
+        with revit.Transaction("Schema Test Set Settings"):
             try:
                 data = extensible_storage.SetSchemaMapData(
                     schema,
@@ -34,32 +35,48 @@ if __name__ == "__main__":
                 assert data["Key2"] == "Value2"
                 OUTPUT.print_md(":white_heavy_check_mark: SetSchemaMapData succeeded.")
             except Exception as e:
-                OUTPUT.print_md(":cross_mark: SetSchemaMapData failed .\n{}".format(e))
+                OUTPUT.print_md(":cross_mark: SetSchemaMapData failed. {}".format(e))
             try:
-                extensible_storage.SetFlamingoSetting("TestSetting", "TestValue", doc)
+                extensible_storage.SetFlamingoSetting(
+                    "TestSetting", "TestValue", doc=doc, schema=schema
+                )
                 OUTPUT.print_md(
                     ":white_heavy_check_mark: SetFlamingoSetting succeeded."
                 )
             except Exception as e:
-                OUTPUT.print_md(
-                    ":cross_mark: SetFlamingoSetting failed .\n{}".format(e)
-                )
+                OUTPUT.print_md(":cross_mark: SetFlamingoSetting failed. {}".format(e))
+            doc.Regenerate()
+        with revit.Transaction("Schema Test Get Settings"):
             try:
-                value = extensible_storage.GetFlamingoSetting("Key1", doc=doc)
-                assert value == "Value1"
-                value = extensible_storage.GetFlamingoSetting("TestSetting", doc=doc)
-                assert value == "TestValue"
-                defaultValue = extensible_storage.GetFlamingoSetting(
-                    "NonExistingSetting", default="DefaultValue", doc=doc
+                data = extensible_storage.GetSchemaMapData(
+                    schema, "Settings", projectInfo
                 )
-                assert defaultValue == "DefaultValue"
+                assert (
+                    data["Key1"] == "Value1"
+                ), "GetSchemaMap Expected 'Value1', got '{}'".format(data["Key1"])
+                value = extensible_storage.GetFlamingoSetting(
+                    "Key1", doc=doc, schema=schema
+                )
+                assert (
+                    value == "Value1"
+                ), "GetFlamingoSettings Expected 'Value1', got '{}'".format(value)
+                value = extensible_storage.GetFlamingoSetting(
+                    "TestSetting", doc=doc, schema=schema
+                )
+                assert value == "TestValue", "Expected 'TestValue', got '{}'".format(
+                    value
+                )
+                defaultValue = extensible_storage.GetFlamingoSetting(
+                    "NonExistingSetting", default="DefaultValue", doc=doc, schema=schema
+                )
+                assert (
+                    defaultValue == "DefaultValue"
+                ), "Expected 'DefaultValue', got '{}'".format(defaultValue)
                 OUTPUT.print_md(
                     ":white_heavy_check_mark: GetFlamingoSetting succeeded."
                 )
             except Exception as e:
-                OUTPUT.print_md(
-                    ":cross_mark: GetFlamingoSetting failed .\n{}".format(e)
-                )
+                OUTPUT.print_md(":cross_mark: GetFlamingoSetting failed. {}".format(e))
     except Exception as e:
         LOGGER.exception("Error during schema test:\n{}".format(e))
     # finally:
